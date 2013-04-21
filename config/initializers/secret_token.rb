08589af7f1536a8d5a5a80dd9d12d949d8b96c99
Matt Chun-Lum <mchunlum@gmail.com>
2013-04-21 16:41:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RobocodeMatchmaker::Application.config.secret_token = '7d47ee9db335f2b8b52ded6a515bb836e537a1a234ba0326f1de87af2cf78a252c811abe0b96ac6a83457b21bb8fdd5d4e95de5eda4f4624319454536fb2f6b6'
